79f054af8ab31bb54aec24e76636d957ba471557
MasyJordan <masyjordan@gmail.com>
2025-08-14 09:55:32 +0200

    add files .gitignore

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,3 +1,7 @@
+using DefaultNamespace;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
